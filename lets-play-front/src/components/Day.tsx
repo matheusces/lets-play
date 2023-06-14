83e550392aee17df21fbad63c1962bdacb702949
matheuscesar <matheuscesar@neoron.io>
2023-06-13 23:34:35 -0300

    last changes

--- lets-play-front/src/components/Day.tsx
+++ lets-play-front/src/components/Day.tsx
@@ -1,28 +1,29 @@
-import { useState, useContext } from 'react';
-import DayContext from '../contexts/PanelContext';
+import { useState, useContext } from "react";
+import DayContext from "../contexts/PanelContext";
 
 interface DayProps {
-  day: string
+  day: string;
 }
 
 function Day({ day }: DayProps) {
   const { isDaySelected, setIsDaySelected } = useContext(DayContext);
   const { selectedDay, setSelectedDay } = useContext(DayContext);
 
-
   function handleDaySelection() {
     setSelectedDay(day);
     setIsDaySelected(true);
   }
 
-
   return (
     <>
-      <button className="w-20 h-20 bg-color-0 flex text-black items-center justify-center hover:drop-shadow-primary" onClick={(handleDaySelection)}>
-        {day} 
+      <button
+        className="w-20 h-20 bg-color-0 flex text-black items-center justify-center hover:drop-shadow-primary"
+        onClick={handleDaySelection}
+      >
+        {day}
       </button>
     </>
-  )
+  );
 }
 
-export default Day
+export default Day;
